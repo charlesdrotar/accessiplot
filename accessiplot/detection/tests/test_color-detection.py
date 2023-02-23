cce3ab2b51a1de9078ddf314498b51e1b0cf157c
yadiw <yadiw@uw.edu>
2023-02-23 09:26:41 -0800

    added tests for color detection (without anything in them)

---
+++ accessiplot/detection/tests/test_color-detection.py
@@ -0,0 +1,26 @@
+import pytest
+import numpy as np
+from matplotlib.colors import to_rgb
+from matplotlib import pyplot as plt
+from accessiplot.detection.coloe_detection import *
+
+def test_get_common_colors_from_image():
+    assert True
+
+
+def test_get_common_colors_from_plot():
+    assert True
+    
+    
+def test_convert_image():
+    assert True
+
+
+def test_display_image():
+    assert True
+
+
+def test_compare_colors():
+    assert True
+    
+
