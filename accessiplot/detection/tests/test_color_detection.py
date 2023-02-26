be38842f893f0fcb6fb6707d446a538fdcde729d
yadiw <yadiw@uw.edu>
2023-02-26 14:04:20 -0800

    fixed file anme

--- accessiplot/detection/tests/test_color_detection.py
+++ accessiplot/detection/tests/test_color_detection.py
@@ -2,10 +2,20 @@ import pytest
 import numpy as np
 from matplotlib.colors import to_rgb
 from matplotlib import pyplot as plt
-from accessiplot.detection.coloe_detection import *
+
+import sys
+
+from accessiplot.detection.color_detection import *
 
 def test_get_common_colors_from_image():
-    assert True
+    # data to be plotted
+    x = np.arange(1, 11)
+    y = np.array([100, 10, 300, 20, 500, 60, 700, 80, 900, 100])
+    num_lines = 10
+    for i in range(num_lines):
+        y_val = (np.random.rand(1,10)).T
+        plt.plot(x, y_val)
+    plt.show()
 
 
 def test_get_common_colors_from_plot():
@@ -24,3 +34,4 @@ def test_compare_colors():
     assert True
     
 
+test_get_common_colors_from_image()
