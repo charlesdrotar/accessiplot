dc628716784748436b56ca62894130cb098c8ba6
charlesdrotar <drotarcharles@gmail.com>
2023-03-09 22:16:34 -0800

    Fix typo

--- examples/example_detect_all_inaccessibilities.py
+++ examples/example_detect_all_inaccessibilities.py
@@ -25,5 +25,5 @@ for _ in range(num_lines):
 dh = DetectionHandler(ax=ax)
 dh.run_detections(run_detections_list=DetectionTypes.ALL())
 
-for k, v in dh.detections:
-    print(k, ":", v)
+for key in dh.detections.keys():
+    print(key, ":", dh.detections[key])
